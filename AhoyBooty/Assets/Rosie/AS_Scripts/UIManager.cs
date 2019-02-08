126e4848dab3a5cf40380c358aceed49c4af9b6b
RosieMable <sciarrillo.nella@gmail.com>
2019-02-08 12:55:49 +0000

    Added Start Scene
    Started making scripts for the start scene

---
+++ AhoyBooty/Assets/Rosie/AS_Scripts/UIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+public class UIManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class UIObjects
+    {
+        public GameObject UI_Object;
+    }
+    public List<UIObjects> _UIObjects = new List<UIObjects>();
+
+
+
+    [SerializeField]
+    private int SceneIndex;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+    public void ShowUIObjects(int indexUIObject)
+    {
+        _UIObjects[indexUIObject].UI_Object.gameObject.SetActive(true);
+    }
+
+    public void HideUIObjects(int indexUIObject)
+    {
+        _UIObjects[indexUIObject].UI_Object.gameObject.SetActive(false);
+    }
+
+}
+
